d487bead1905bc01596b0e6251e268a5fdc7db48
kjeffreys <kjeffreyscs@gmail.com>
2023-08-29 14:32:35 -0400

    Removed warnings useEffect for netlify deploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import data from './data.json';
 import PositionSelector from './PositionSelector';
 import PlayerList from './PlayerList';
