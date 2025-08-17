5c0a1f02e4a481010a76cd2acf29633e3ec1959c
Kyle Jeffreys <kjeffreyscs@gmail.com>
2025-08-16 20:32:52 -0400

    All configs should be correct, still might need tuning but league #s should be right pending error

--- backend/cli.py
+++ backend/cli.py
@@ -98,11 +98,11 @@ def all(ctx):
     log.info("CLI: Running all pipeline phases.")
     date = ctx.obj["date"]
     try:
-        # log.info("--- Phase 1: Ingest ---")
-        # # run_ingest(date_str=date)
+        log.info("--- Phase 1: Ingest ---")
+        run_ingest(date_str=date)
 
-        # log.info("--- Phase 2: Clean ---")
-        # run_clean(date_str=date)
+        log.info("--- Phase 2: Clean ---")
+        run_clean(date_str=date)
 
         log.info("--- Phase 3: Enrich ---")
         run_enrich(date_str=date)
